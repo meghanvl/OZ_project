8523fd9969e7fdf057631aef255a8f953be4bce0
kirby10320 <72478796+kirby10320@users.noreply.github.com>
2021-03-13 14:27:13 -0600

    D3 updates

--- leaflet/static/js/heatmap.js
+++ leaflet/static/js/heatmap.js
@@ -1,7 +1,6 @@
 var myMap = L.map("map", {
   center: [37.0902, -95.7129],
-  zoom: 5
-});
+  zoom: 20
 
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
